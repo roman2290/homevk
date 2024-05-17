c5c04695837f0565ccaab5542a96a5ea99b87a8d
Roman <Roman.22.04@yandex.ru>
2024-05-17 16:33:07 +0700

    Initial commit

--- homevk.py
+++ homevk.py
@@ -1,6 +1,8 @@
 import requests
 import pprint
-
+import json
+from tqdm import trange, tqdm
+from time import sleep
 
 
 
@@ -20,22 +22,30 @@ class VK:
         return response.json()
     
     # Получение и закрузка фото с профиля
-    def foto_project(self, owner_id, count = 1):
+    def foto_project(self, owner_id, offset = 0, count = 4):
+        self.offset = offset,
         self.count = count,
         self.owner_id = owner_id
         url_vk = 'https://api.vk.com/method/photos.get'
-        params_vk = {'owner_id': self.owner_id,
-                'count': 1,
+        params_vk = {
+                'owner_id': owner_id,
+                'count': count,
                 'photo_sizes': 0,
                 'album_id': 'profile',
-                'extended': 1}
+                'extended': 1,
+                'offset': offset}
         response = requests.get(url_vk, params={**self.params, **params_vk, **headers})
-        image_vk = response.json()['sizes']
-        response = requests.get(image_vk)
+        image_vk = response.json()['response']['items'][3]['sizes'][3]['url'][3]
+        for file in response:
+            image_vk = file['sizes'][-1]['url']
+            file_name = image_vk.split('/')[-1]
+
         with open('image.jpg', 'wb') as f:
+            response = requests.get(file_name)
             f.write(response.content)
-        return response.json()
-    
+        return response
+
+
 class Yandex:
     #создаие папки на ядиске
     def __init__(self, path):
@@ -43,24 +53,28 @@ class Yandex:
    
     def foto_yandex_disk(self):
         url_create_folder = 'https://cloud-api.yandex.net/v1/disk/resources'
-        params = {"path": "Image"}
-        headers = {"Authorization": ""}
+        params = {"path": 'Image'}
+        headers = {"Authorization": "OAuth y0_AgAAAAAOk2cQAADLWwAAAAEDecVcAACa71RZu3dKkKhY9AElp54ZOVdl0w"}
         response = requests.put(url_create_folder, params=params, headers=headers)
-        return response
+        print(response.json())
     
     def foto_upload(self, foto_folder):
         self.foto_folder = foto_folder
         self.params = {'foto_folder': self.foto_folder}
         url_load = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-        params = {'path': f'Image/{foto_folder}'}
+        params = {"path": f'{"Image"}/{foto_folder}'}
         response = requests.get(url_load, params=params, headers=headers)
+        with open('image.jpg', 'rb') as f:
+            url_upload = response.json()['href']
+            requests.put(url_upload, files={'file': f})
         
-        url_image = response.json()['url']
-        self.image_name = url_image.split('/')[-1]
-        with open(file_name, 'rb') as f:
-                url_upload = response['href']
-                requests.put(url_upload, files={'file': f})
-        return (response.json())
+        for img in tqdm(url_upload):
+            print(img)
+        
+        
+        
+        
+        
     
  
 
@@ -72,16 +86,22 @@ vk = VK(access_token, user_id, owner_id)
 token = ''
 
 headers = {'Authorization': f'OAuth {token}'}
-path={'Image'}
+
+
+
+
 foto_folder = {'Image'}
 url_upload = {'Image'}
 Image = {'Image'}
 file_name = ('Image')
-
+path={f'Image/{foto_folder}'}
 yandex = Yandex(path)
+foto_folder = ['href']
 
-print(yandex.foto_yandex_disk())
-pprint.pprint(yandex.foto_upload(foto_folder))
+pprint.pprint(yandex.foto_yandex_disk())
+print(yandex.foto_upload(foto_folder))
 pprint.pprint(vk.users_info())
-pprint.pprint(vk.foto_project(owner_id))
+pprint.pprint(vk.foto_project(owner_id, count = 4))
+
+  
 
